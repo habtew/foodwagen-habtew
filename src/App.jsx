ff8a779f337bc7b9906e6e2f8844872dc39686f2
habtew <habtamuwolde92@gmail.com>
2025-08-10 18:33:39 +0530

    fixing naming issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import Header from './components/header/Header'
+import Header from './components/Header/Header'
 import Hero from './components/hero/Hero'
 import Main from './components/Main/Main'
 import Footer from './components/Footer/Footer'
